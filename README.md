3a857464e93c7fcd53edd61ee2a52951583268df
Daniel Shaw <dashaw92@gmail.com>
2016-02-09 23:31:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TShock plugins
+This is a collection of my TShock plugins that I will be making.
+
+I will be using this repo to not only provide public examples of plugins for the modern API, but 
+also (hopefully) learn more myself for a project I have been planning for a while.
+
+# Links
+Official Terraria website: https://terraria.org/
+
+TShock website: https://tshock.co/
+
+# License
+All code here is subject to the MIT license - see LICENSE.MD.
+
+I do not own Terraria
---
+++ TShock_Hello/TShock_Hello/HelloWorld.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//These are required to make a plugin
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace TShock_Hello
+{
+    //1.3.0.8
+    [ApiVersion(1, 22)]
+    public class HelloWorld : TerrariaPlugin
+    {
+
+        public HelloWorld(Main game) : base(game) { }
+
+        public override string Name { get { return "Hello World"; } }
+        public override string Author { get { return "Daniel Shaw"; } }
+        public override string Description { get { return base.Description; } }
+        public override Version Version { get { return new Version(1, 0); } }
+
+
+        public override void Initialize() {
+            TShockAPI.GetDataHandlers.PlayerInfo += PlayerInfo;
+        }
+
+        void PlayerInfo(object sender, TShockAPI.GetDataHandlers.PlayerInfoEventArgs args) {
+            Console.WriteLine(args.Name + " just called a PlayerInfo event.");
+            Console.WriteLine(args.Name + " has " + args.Hair + " hair!");
+        }
+    }
+}
